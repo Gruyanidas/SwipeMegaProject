a35e23c1f2c1bb90ed54a9165e8dd8a28f296ae6
gruyanidas <milosgrujic1987@gmail.com>
2023-05-20 23:22:08 +0200

    Project fcked up because of fail in aproach of responsive design - closed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,9 @@ module.exports = {
   content: ["./public/**/*.html"],
   theme: {
     screens: {
-      "3xl": { max: "1800px" },
-      "2xl": { max: "1535px" },
-      xl: { max: "1279px" },
-      lg: { max: "1023px" },
-      md: { max: "767px" },
-      xs: { max: "400px" },
+      // "3xl": { max: "1800px" },
+      // "md": { max: "767px" },
+      "xs": { max: "400px" },
     },
     extend: {
       fontFamily: {
